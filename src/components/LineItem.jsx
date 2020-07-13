de170b61078abe3da730eb2e729f84acb079d930
Stefani Baker <stefanibaker@MacBook-Air-4.local>
2020-07-13 18:15:54 -0400

    Still working on addApples strangeness

--- src/components/LineItem.jsx
+++ src/components/LineItem.jsx
@@ -5,19 +5,17 @@ function LineItem(props) {
     return (
      
         <div>
-            <table>
-                <thead>
+            <table className="itemTable">
+                <tr>
                     <th>Item Name</th>
                     <th>Item Cost</th>
                     <th>Item Quantity</th>
-                </thead>
+                </tr>
                 <tr>
                     <td>{props.name}</td>
                     <td>{props.cost}</td>
                     <td>{props.quantity}</td>
                 </tr>
-                <tfoot>
-                </tfoot>
             </table>
         </div>
     )
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LineItem from "./LineItem";
+import Totals from "./Totals"
 
 export default class ShoppingCart extends Component {
     constructor(){
@@ -9,16 +10,25 @@ export default class ShoppingCart extends Component {
             applesCost : 1,
             orangesQuantity : 0,
             orangesCost : 2,
+            tax : 0,
+            subtotal : 0,
             total : 0
         }
     }
 
+
+
     addApples = () => {
-        console.log("Inside ShoppingCart.addApple");
+        let totaltemp = this.state.subtotal + this.state.applesCost;
+        let appleQuTemp = this.state.applesQuantity + 1;
+        console.log("******** Inside ShoppingCart.addApple ********");
         this.setState({
-            applesQuantity : this.state.applesQuantity + 1,
-            total : this.state.total + this.state.applesCost
-        })
+            applesQuantity: appleQuTemp,
+            subtotal: totaltemp
+        });
+        console.log("this.state.applesQuantity: ",this.state.applesQuantity);
+        console.log("this.state.subtotal: ",this.state.subtotal);
+        this.calculateTax();
     }
 
     minusApples = () => {
@@ -26,8 +36,9 @@ export default class ShoppingCart extends Component {
         if (this.state.applesQuantity !== 0) {
         this.setState({
             applesQuantity : this.state.applesQuantity - 1,
-            total : this.state.total - this.state.applesCost
+            subtotal : this.state.subtotal - this.state.applesCost
         })            
+        this.calculateTax();
         }
     }
 
@@ -35,16 +46,20 @@ export default class ShoppingCart extends Component {
         console.log("Inside ShoppingCart.addApple");
         this.setState({
             orangesQuantity : this.state.orangesQuantity + 1,
-            total : this.state.total + this.state.orangesCost
+            subtotal : this.state.subtotal + this.state.orangesCost
         })
+        this.calculateTax();
     }
 
     minusOranges = () => {
         console.log("Inside ShoppingCart.minusApple");
-        this.setState({
-            orangesQuantity : this.state.orangesQuantity - 1,
-            total : this.state.total - this.state.orangesCost
-        })
+        if (this.state.orangesQuantity !== 0) {
+            this.setState({
+                orangesQuantity : this.state.orangesQuantity - 1,
+                subtotal : this.state.subtotal - this.state.orangesCost
+            })
+        this.calculateTax();
+        }
     }
 
     addGiftWrap = (e) =>{
@@ -52,7 +67,19 @@ export default class ShoppingCart extends Component {
     }
 
     calculateTax = () => {
-       return (this.state.total !== 0) ? (this.state.total + (this.state.total * 0.08)).toFixed(2) : 0.00
+        console.log("******* Inside calculate Tax *******");
+        console.log("this.state.subtotal: ",this.state.subtotal);
+        if (this.state.subtotal !== 0) {
+            this.setState({
+                tax : this.state.subtotal * 0.08,
+                total : this.state.subtotal + this.state.tax
+            })
+        } else {
+            this.setState({
+                tax : 0
+            })
+        }
+
     }
 
 
@@ -60,16 +87,18 @@ export default class ShoppingCart extends Component {
         return (
             <div className="shoppingCart">
                 <LineItem name = "Apples" cost = "$1.00" quantity = {this.state.applesQuantity}/>
-                <button id="plusApples" onClick={this.addApples}>+</button>
-                <button id="minusApples" onClick={this.minusApples}>-</button>
-                <input type="checkbox" id="giftApple" name="giftApple" value="giftWrapApple"/>
+                <div className="buttonActions">
+                    <button id="plusApples" onClick={this.addApples}>+</button>
+                    <button id="minusApples" onClick={this.minusApples}>-</button>
+                    <input type="checkbox" id="giftApple" name="giftApple" value="giftWrapApple"/>
+                </div>
                 <LineItem name = "Oranges" cost = "$2.00" quantity = {this.state.orangesQuantity}/>
-                <button id="plusOranges" onClick={this.addOranges}>+</button>
-                <button id="minusOranges" onClick={this.minusOranges}>-</button>
-                <input type="checkbox" onChange={this.addGiftWrap} id="giftOrange" name="giftOrange" value="giftWrapOrange"/>
-                <h1>Subtotal: ${this.state.total.toFixed(2)} </h1>
-                <h1>Tax: ${this.calculateTax}</h1>
-
+                <div className="buttonActions">
+                    <button id="plusOranges" onClick={this.addOranges}>+</button>
+                    <button id="minusOranges" onClick={this.minusOranges}>-</button>
+                    <input type="checkbox" onChange={this.addGiftWrap} id="giftOrange" name="giftOrange" value="giftWrapOrange"/>
+                </div>
+                <Totals subtotal={this.state.subtotal} tax={this.state.tax} total={this.state.total} />
             </div>
         )
     }
